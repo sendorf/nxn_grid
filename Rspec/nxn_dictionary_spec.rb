b19e680b1f71e3127052d82ad89af8a8da56d162
Sergio Díaz <sendorf@gmail.com>
2014-03-04 18:38:34 +0100

    Created the rspec for finding words

--- Rspec/nxn_dictionary_spec.rb
+++ Rspec/nxn_dictionary_spec.rb
@@ -10,3 +10,13 @@ describe Nxn_Dictionary, "Generate NxN grid" do
     grid[0].gsub(/\s+/, "").length.should eq(dimension)    			#Checks the second dimension
   end
 end
+
+describe Nxn_Dictionary, "Find in dictionary" do
+  it "Horizontal words from left to right in your grid" do
+    nxn_dictionary = Nxn_Dictionary.new
+    dimension = (2..12).to_a.shuffle.first 							#Random dimension between 2 and 12
+    grid = nxn_dictionary.generate(dimension)
+    words = nxn_dictionary.horizontal_l2r(grid)
+    nxn_dictionary.horizontal_l2r(grid).should eq(words) 				#Checks the output
+  end
+end
